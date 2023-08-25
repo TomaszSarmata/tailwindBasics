4916bb789edbdd6c5c8d1c46566e9109119a9d68
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-25 15:03:01 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        CHESTNUT: "#973f29",
+      },
+    },
+  },
+  plugins: [],
+};
